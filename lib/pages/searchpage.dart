eda4a451ec6e77d28c0b851f85e944c20d4861e4
chan <chanmyae1783@gmail.com>
2024-02-08 23:54:00 -0800

    Committing local changes to searchpage.dart

--- lib/pages/searchpage.dart
+++ lib/pages/searchpage.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:slack/components/row_template.dart';
 
 class SearchPage extends StatelessWidget {
+
   const SearchPage({super.key});
 
   @override
