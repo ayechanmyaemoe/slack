701ce76454393a1ba5113bbaf130824af80f4323
chan <chanmyae1783@gmail.com>
2024-02-09 00:14:18 -0800

    hide and show buttons

--- lib/components/message_box.dart
+++ lib/components/message_box.dart
@@ -3,20 +3,52 @@
 import 'package:flutter/material.dart';
 import 'package:slack/components/message_box_icons.dart';
 
-class MessageBox extends StatelessWidget {
+class MessageBox extends StatefulWidget {
   const MessageBox({super.key});
 
   @override
+  State<MessageBox> createState() => _MessageBoxState();
+}
+
+class _MessageBoxState extends State<MessageBox> {
+
+  FocusNode _focusNode = FocusNode();
+  bool _isTextFieldFocused = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {
+      setState(() {
+        _isTextFieldFocused = _focusNode.hasFocus;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        prefixIcon: Icon(Icons.add),
-        hintText: 'Messages #social-media',
-        suffixIcon: Icon(Icons.mic),
-      ),
-      onTap: () {
-        Visibility(child: MessageBoxIcons(), visible: true,);
-      },
+    return Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.add),
+            hintText: 'Messages #social-media',
+            suffixIcon: Icon(Icons.mic),
+          ),
+          focusNode: _focusNode,
+        ),
+        Visibility(
+          visible: _isTextFieldFocused,
+          child: MessageBoxIcons()
+        )
+      ],
     );
   }
 }
